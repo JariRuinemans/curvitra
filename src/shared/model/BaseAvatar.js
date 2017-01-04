c57380348c54acde01965de79a89f68f2fe0ccdc
Jari Ruinemans <jariruinemans@gmail.com>
2017-01-04 20:22:38 +0100

    Revert "asfasf"
    This reverts commit 2eb3f4e43510b80bc54f04971132216dfa14ea73.

--- src/shared/model/BaseAvatar.js
+++ src/shared/model/BaseAvatar.js
@@ -92,7 +92,10 @@ BaseAvatar.prototype.directionInLoop = true;
  function sp1(){
 	 BaseAvatar.prototype.speed = true;
  }
-
+  function sp2(){
+	 BaseAvatar.prototype.speed = false;
+ }
+ 
 BaseAvatar.prototype.equal = function(avatar)
 {
     return this.id === avatar.id;
